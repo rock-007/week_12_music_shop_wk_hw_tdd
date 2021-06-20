b4b805aa952a5a06c1efec17321ed58e7f2c5a78
rock-007 <umair.ashraf2019@gmail.com>
2021-06-21 00:30:21 +0100

    ShopTest file added

--- src/main/java/shop/Shop.java
+++ src/main/java/shop/Shop.java
@@ -1,4 +1,36 @@
 package shop;
 
+import behaviours.ISell;
+
+import java.util.HashMap;
+
 public class Shop {
+    HashMap<ISell, Integer> stocklevel;
+
+    public Shop() {
+        this.stocklevel = new HashMap<>();
+    }
+
+    public HashMap<ISell, Integer> getStocklevel() {
+        return stocklevel;
+    }
+
+    public int currentStockQuantity(ISell stockItem) {
+        return stocklevel.getOrDefault(stockItem, 0);
+    }
+
+
+    public void setStocklevel(ISell stockItem, int quantity) {
+        this.stocklevel.put(stockItem, currentStockQuantity(stockItem) + quantity);
+    }
+
+    public void sellItemFromStock(ISell stockItem, int quantity) {
+        if (currentStockQuantity(stockItem) == 1) {
+            this.stocklevel.remove(stockItem);
+        } else if (currentStockQuantity(stockItem) > 1) {
+            this.stocklevel.put(stockItem, currentStockQuantity(stockItem) - 1);
+
+        }
+    }
+
 }
